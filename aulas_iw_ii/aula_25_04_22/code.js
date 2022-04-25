81d1e021bf0ff34811c5b84045015dc1a8ebe26b
Henrique Cardoso Lima <79847872+henriqlimac@users.noreply.github.com>
2022-04-25 09:08:44 -0300

    Add files via upload

---
+++ aulas_iw_ii/aula_25_04_22/code.js
@@ -0,0 +1,4 @@
+console.log('Testando Plugin');
+//Tecla "F8"
+
+//Esse "code.js" é apenas para testar o plugin "Node.js Exec".
---
+++ aulas_iw_ii/aula_25_04_22/code3.js
@@ -0,0 +1,8 @@
+var pais = 'EUA'
+
+console.log(`Nasci em ${pais} e vivo em ${pais}.`)
+if (pais == 'BRA'){
+    console.log(`Você é brasileiro.`)
+} else {
+    console.log(`Você é estrangeiro.`)
+}
