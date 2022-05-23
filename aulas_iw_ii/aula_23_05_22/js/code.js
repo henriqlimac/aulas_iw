f2ab6478e269ec7d78dd2e6dfbbaa11a5569a2df
Henrique Cardoso Lima <79847872+henriqlimac@users.noreply.github.com>
2022-05-23 09:03:44 -0300

    Add files via upload

---
+++ aulas_iw_ii/aula_23_05_22/js/code.js
@@ -0,0 +1,27 @@
+botao = document.getElementById('botao')
+lamp = document.getElementById('lampada')
+text = document.getElementById('text')
+
+botao.onclick = function(){
+    if(botao.value == 'acender') {
+        lamp.src = './imgs/acesa.png'
+        botao.value = 'apagar'
+        botao.innerHTML = 'Apagar'
+        document.body.style.backgroundColor = 'white';
+        text.style.color = 'black';
+        botao.style.backgroundColor = 'black';
+        botao.style.color = 'white';
+        botao.style.borderColor = 'white';
+    } else {
+            lamp.src = './imgs/apagada.png'
+            botao.value = 'acender'
+            botao.innerHTML = 'Acender'
+            document.body.style.backgroundColor = 'black';
+            text.style.color = 'white';
+            botao.style.backgroundColor = 'white';
+            botao.style.color = 'black';
+            botao.style.borderColor = 'black';
+    }
+
+}
+
